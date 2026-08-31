718517e049fdd3cbf1d3c2c41c09f185938d9a26
2nyunuss-coder <2n.yunuss@gmail.com>
2026-08-31 23:18:39 +0300

    Improve YEA V0.5 offline cache

--- yea-suite/sw.js
+++ yea-suite/sw.js
@@ -1,5 +1,5 @@
-const CACHE='yea-suite-v05';
-const CORE=['./','./manifest.webmanifest','./icon.svg'];
+const CACHE='yea-suite-v05-2';
+const CORE=['./v05.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
@@ -7,7 +7,7 @@ self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
   if(url.origin!==self.location.origin)return;
   if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./',copy));return r}).catch(()=>caches.match('./')));
+    event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./v05.html',copy));return r}).catch(()=>caches.match('./v05.html')));
     return;
   }
   event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r})));
