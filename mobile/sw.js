56b899faf696f1107e98a989256325cd8401eedc
2nyunuss-coder <2n.yunuss@gmail.com>
2026-09-04 19:27:25 +0300

    RPYS Cep v2: update sw.js

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,32 +1,5 @@
-const CACHE = 'rpys-cep-shell-v2';
-const SHELL = ['./', './index.html', './mobile.js', './manifest.webmanifest', './icon.svg'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key.startsWith('rpys-cep-shell-') && key !== CACHE).map((key) => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin || !url.pathname.includes('/mobile/')) return;
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response.ok) {
-          const copy = response.clone();
-          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
-  );
-});
+const CACHE = 'rpys-cep-shell-v3';
+const SHELL = ['./', './index.html', './mobile.css', './mobile.js', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; const url = new URL(event.request.url); if (url.origin !== self.location.origin) return; if (url.pathname.includes('/mobile/')) event.respondWith(fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE).then(cache => cache.put(event.request,copy)); return response; }).catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))); });
